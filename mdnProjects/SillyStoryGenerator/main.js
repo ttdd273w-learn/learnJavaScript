5009dedcc069a027a9f33036bed3bed6d4b4e061
Tong Duan <td273@cornell.edu>
2022-11-21 10:01:41 -0500

    Finished story generator

---
+++ mdnProjects/SillyStoryGenerator/main.js
@@ -0,0 +1,54 @@
+// 1. COMPLETE VARIABLE AND FUNCTION DEFINITIONS
+
+const customName = document.getElementById("customname");
+const randomize = document.querySelector(".randomize");
+const story = document.querySelector(".story");
+
+function randomValueFromArray(array) {
+  const random = Math.floor(Math.random() * array.length);
+  return array[random];
+}
+
+// 2. RAW TEXT STRINGS
+
+let storyText = `It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.`;
+
+let insertX = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
+
+let insertY = ["the soup kitchen", "Disneyland", "the White House"];
+
+let insertZ = [
+  "spontaneously combusted",
+  "melted into a puddle on the sidewalk",
+  "turned into a slug and crawled away",
+];
+
+// 3. EVENT LISTENER AND PARTIAL FUNCTION DEFINITION
+
+randomize.addEventListener("click", result);
+
+function result() {
+  let newStory = storyText;
+  let xItem = randomValueFromArray(insertX),
+    yItem = randomValueFromArray(insertY),
+    zItem = randomValueFromArray(insertZ);
+
+  newStory = newStory.replaceAll(":insertx:", xItem);
+  newStory = newStory.replaceAll(":inserty:", yItem);
+  newStory = newStory.replaceAll(":insertz:", zItem);
+
+  if (customName.value !== "") {
+    const name = customName.value;
+    newStory = newStory.replaceAll("Bob", name);
+  }
+
+  if (document.getElementById("uk").checked) {
+    const weight = Math.round(300 * 0.0714286) + " stone";
+    const temperature = Math.round((94 - 32) * (5 / 9)) + " centigrade";
+    newStory = newStory.replace("300 pounds", weight);
+    newStory = newStory.replace("94 fahrenheit", temperature);
+  }
+
+  story.textContent = newStory;
+  story.style.visibility = "visible";
+}
